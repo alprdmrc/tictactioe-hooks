dfe61344fac6a06d448f0134ea704c12d5d5a3a0
alper demirci <afd@alpers-MacBook-Pro.local>
2020-02-17 19:37:20 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,131 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Board(props) {
+    function renderSquare(i) {
+        return (
+            <Square
+                value={props.squares[i]}
+                onClick={() => props.onClick(i)}
+            />
+        );
+    }
+
+    return (
+        <div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+
+    );
+}
+
+function Game() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([squares]);
+    const [stepNumber, setStepNumber] = useState(1);
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner: '+ winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
+    const moves = history.map((step, move) => {
+       const desc = move ?
+           'Go to move #' + move :
+           'Go to game start';
+       return (
+           <li key={move}>
+               <button onClick={() => jumpTo(move)}>{desc}</button>
+           </li>
+       );
+    });
+    function jumpTo(step) {
+        setStepNumber(step);
+        setXIsNext(step % 2 === 0);
+    }
+
+    function handleClick(i) {
+        const nextSquares = squares.slice();
+        console.log(nextSquares);
+        console.log(squares);
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        nextSquares[i] = xIsNext ? 'X' : 'O';
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+        setHistory([...history, nextSquares]);
+        setStepNumber(history.length + 1);
+        console.log(history);
+        console.log(stepNumber);
+    }
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+                return squares[a];
+            }
+        }
+        return null;
+    }
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    squares = {squares}
+                    onClick = {(i) => handleClick(i)}
+                />
+            </div>
+            <div className="game-info">
+                <div className="status">{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
+
+
+
